1645f033787b24e24af632eaf2af41944d2b20b9
Mitchell Combs <mitch.combs.92@gmail.com>
2018-05-24 12:27:07 -0400

    Added example of add index command

--- PRS SQL User table creation sample.sql
+++ PRS SQL User table creation sample.sql
@@ -1,6 +1,10 @@
+--create database PRSDatabase
+
+--drop table [user]
+
 create table [User] (
 	ID int not null primary key identity(1,1),
-	Username nvarchar(30) unique not null,
+	Username nvarchar(30) not null,
 	Password nvarchar(30) not null,
 	Firstname nvarchar(30) not null,
 	Lastname nvarchar(30) not null,
@@ -10,3 +14,7 @@ create table [User] (
 	IsAdmin bit not null default 0,
 	Active bit not null default 1
 )
+
+--select * from [user]
+
+--create unique index uix_Username on [user](Username);
