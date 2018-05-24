6de64f3c69e991e10d2e9247d66c9959873a0650
Mitchell Combs <mitch.combs.92@gmail.com>
2018-05-24 11:48:25 -0400

    Initial PRS SQL commit

---
+++ PRS SQL User table creation sample.sql
@@ -0,0 +1,12 @@
+create table [User] (
+	ID int not null primary key identity(1,1),
+	Username nvarchar(30) unique not null,
+	Password nvarchar(30) not null,
+	Firstname nvarchar(30) not null,
+	Lastname nvarchar(30) not null,
+	Phone nvarchar(12) not null,
+	Email nvarchar(255) not null,
+	IsReviewer bit not null default 0,
+	IsAdmin bit not null default 0,
+	Active bit not null default 1
+)
